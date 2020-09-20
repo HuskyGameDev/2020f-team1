4287f8e692659c9be3a1dbb15ae06ae3108858f8
jacob322 <jacob322@gmail.com>
2020-09-20 14:38:23 -0400

    projectiles (base class)
    set up projectile base

--- CombatZone/weapons/bullets/projectiles.gd
+++ CombatZone/weapons/bullets/projectiles.gd
@@ -1,19 +1,33 @@
 extends Area2D
 
 export var speed = 100
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+export var damage = 50
+
+var velocity:= Vector2.ZERO
+#var speed: = 0
+var rng: = RandomNumberGenerator.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+    rng.randomize()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#    pass
-
+func start_at(dir, pos) -> void:
+    rotation = dir
+    position = pos
+    
+    velocity = Vector2(speed,0).rotated(dir - PI / 2)
+    
+func random_float(spread) -> float:
+    var my_random_num = rng.randf_range(-spread, spread)
+    return my_random_num
+    
 func _physics_process(delta: float) -> void:
     position += transform.x * speed * delta
+
+
+
+func _on_projectiles_body_entered(body: Node) -> void:
+    if body.get_groups().has("flesh_damageable"):
+        queue_free()
+        print(body.get_name())
