626917090b9b3e0f1d6b36540922f9bf8027709b
Rademero <47907410+Rademero@users.noreply.github.com>
2020-10-04 14:08:00 -0400

    Base for plane
    This is a starting point for the plane, it has the plane incress in size and also working forward movement.

--- CombatZone/Environment/TestLevel.gd
+++ CombatZone/Environment/TestLevel.gd
@@ -8,12 +8,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 func _on_weapon_shoot(bullet, pos, dir):
-    var b = bullet.instance()
-    add_child(b)
-    b.start(pos,dir)
+	var b = bullet.instance()
+	add_child(b)
+	b.start(pos,dir)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #    pass
