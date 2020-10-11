254fc5c7a99e81dd5e03eec069fa11096ab12a5d
Rademero <47907410+Rademero@users.noreply.github.com>
2020-10-11 13:43:20 -0400

    Plane update
    This adds the last parts of the plane's movement.

--- CombatZone/vehicle/Plane.gd
+++ CombatZone/vehicle/Plane.gd
@@ -1,27 +1,43 @@
 extends "res://vehicle/base_vehicle.gd"
-
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
-
 func get_input() -> void:
 	var turn = 0
-	
+	#holds origanal values 
+	var defultFri = friction
+	var defultDrag = drag
 	# Turning
 	turn = Input.get_action_strength("vehicle_turn_right") - Input.get_action_strength("vehicle_turn_left")
 	steer_angle = turn * deg2rad(steering_angle)
 	# Acceleration
 	acceleration = transform.x * engine_power * Input.get_action_strength("vehicle_accelerate")
+	#gets the volocity of the plane as a float
+	var InFlight = velocity.dot(velocity.normalized())
 	# Braking
 	if Input.is_action_pressed("vehicle_brake"):
-		acceleration = transform.x * braking * Input.get_action_strength("vehicle_brake")
-	
+		if InFlight < 100: #Plane is on the ground
+			acceleration = transform.x * braking * Input.get_action_strength("vehicle_brake") * .66
+		if InFlight > 120: #Plane is in the air
+			acceleration = transform.x * braking * Input.get_action_strength("vehicle_brake")
+	#checks if the plane is in air
+	if InFlight >= 100:  
+		friction = 0    #no ground friction in air
+		drag = -0.0015
+		scale.x = 1 + .5
+		scale.y = scale.x
+		$CollisionShape2D.scale.x = 1+ .5
+		$CollisionShape2D.scale.y = 1+ .5
+	#on ground
+	if InFlight <=100:
+		#resets all of the values/ scales
+		scale.x = 1
+		scale.y = scale.x
+		friction = defultFri 
+		drag = defultDrag
+		$CollisionShape2D.scale.x = 1
+		$CollisionShape2D.scale.y = 1
+		
 func calculate_steering(delta):
 	var rear_wheel = position - transform.x * wheel_base / 2.0
 	var front_wheel = position + transform.x * wheel_base / 2.0
@@ -41,3 +57,7 @@ func calculate_steering(delta):
 		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
 		
 	rotation = new_heading.angle()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
