c1d06914d292998c59e8cdfff89385439c68188c
jacob322 <jacob322@gmail.com>
2020-10-25 17:06:45 -0400

    changed spacing for editor settings

--- CombatZone/weapons/bullets/projectiles.gd
+++ CombatZone/weapons/bullets/projectiles.gd
@@ -10,37 +10,37 @@ var rng: = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	rng.randomize()
-	print("bullet readied")
+    rng.randomize()
+    print("bullet readied")
 
 func start_at(pos, dir) -> void:
-	# rotation = dir.angle()
-	print("bullet starts")
-	print(pos)
-	rotation = dir
-	position = pos
-	$life_time.wait_time = lifetime
-	velocity = Vector2(speed,0).rotated(dir)
-	#velocity = dir * speed
-	
+    # rotation = dir.angle()
+    print("bullet starts")
+    print(pos)
+    rotation = dir
+    position = pos
+    $life_time.wait_time = lifetime
+    velocity = Vector2(speed,0).rotated(dir)
+    #velocity = dir * speed
+    
 func random_float(spread) -> float:
-	var my_random_num = rng.randf_range(-spread, spread)
-	return my_random_num
-	
+    var my_random_num = rng.randf_range(-spread, spread)
+    return my_random_num
+    
 func _physics_process(delta: float) -> void:
-	#position += transform.x * speed * delta
-	#position = get_global_position() + velocity * delta
-	position += velocity * delta
+    #position += transform.x * speed * delta
+    #position = get_global_position() + velocity * delta
+    position += velocity * delta
 
 
 func _on_projectiles_body_entered(body: Node) -> void:
-	print("bullet hit")
-	if body.get_groups().has("flesh_damageable"):
-		queue_free()
-		print(body.get_name())
-		body.take_damage(global_position, damage)
+    print("bullet hit")
+    if body.get_groups().has("flesh_damageable"):
+        queue_free()
+        print(body.get_name())
+        body.take_damage(global_position, damage)
 
 
 func _on_life_time_timeout() -> void:
-	print("bullet dies")
-	queue_free()    # removes object after timeout
+    print("bullet dies")
+    queue_free()    # removes object after timeout
