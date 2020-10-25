7489a985ecad06071eecee9ff15cdc74374b4350
Samael-M <ssjmilner@gmail.com>
2020-10-25 14:12:10 -0400

    Started AI pathfinding, added test level for AI

--- CombatZone/Environment/TestLevel.gd
+++ CombatZone/Environment/TestLevel.gd
@@ -8,12 +8,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 func _on_weapon_shoot(bullet, pos, dir):
-    var b = bullet.instance()
-    add_child(b)
-    b.start(pos,dir)
+	var b = bullet.instance()
+	add_child(b)
+	b.start(pos,dir)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #    pass
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+			if event.button_index == BUTTON_LEFT and event.pressed:
+				var path = $Navigation2D.get_simple_path($DamageableDummy.position, event.position)
+				$Line2D.points = PoolVector2Array(path)
+				$Line2D.show()
