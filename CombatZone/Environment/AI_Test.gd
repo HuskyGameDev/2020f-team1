f7471da76dbea4a782784653679f5f9179cbcd43
jacob322 <jacob322@gmail.com>
2020-10-25 15:06:03 -0400

    reImported png's as '2D pixel'

--- CombatZone/Environment/AI_Test.gd
+++ CombatZone/Environment/AI_Test.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 func _unhandled_input(event):
-	if event is InputEventMouseButton:
-			if event.button_index == BUTTON_LEFT and event.pressed:
-				var path = $Navigation2D.get_simple_path($DamageableDummy.global_position, get_global_mouse_position())
-				$Line2D.points = PoolVector2Array(path)
-				$Line2D.show()
+    if event is InputEventMouseButton:
+            if event.button_index == BUTTON_LEFT and event.pressed:
+                var path = $Navigation2D.get_simple_path($DamageableDummy.global_position, get_global_mouse_position())
+                $Line2D.points = PoolVector2Array(path)
+                $Line2D.show()
