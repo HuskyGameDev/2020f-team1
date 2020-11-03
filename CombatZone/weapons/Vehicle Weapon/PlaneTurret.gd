fb24b5842393788353bd106c06214f38d07efe8f
Rademero <47907410+Rademero@users.noreply.github.com>
2020-11-03 14:56:20 -0500

    Fixed hitboxes, added turret
    The plane no longer crashes in air, plane can shoot a bullet

---
+++ CombatZone/weapons/Vehicle Weapon/PlaneTurret.gd
@@ -0,0 +1,17 @@
+extends "res://weapons/fire_arm_basic.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	caliber = '9mm'
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
