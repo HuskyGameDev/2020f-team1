90e1a0f1d393185c5d2428e591fd4bc0b4b20833
Eric Belt <ejbelt@mtu.edu>
2021-04-20 00:50:15 -0400

    Fixed issues where enemies spawned would not attack player

--- CombatZone/Levels/Objectives/L1/L1O2Script.gd
+++ CombatZone/Levels/Objectives/L1/L1O2Script.gd
@@ -5,7 +5,6 @@ export(PackedScene) var PreActivateTilemap
 #Preload enemies in order to spawn them in.
 var PreEnemy = preload("res://character/Proto_enemy.tscn")
 var PreBeacon = preload("../base_custom_beacon.tscn")
-var gotPlayer = false
 
 func _OnReady():
     PreActivateTilemap = get_child(0)
@@ -38,27 +37,50 @@ func _OnRevealed():
     if PreActivateTilemap != null:
         PreActivateTilemap.clear()
     
+    #Start at child 1 since child 0 is reserved for the tilemap we clear upon reveeling the objective
     var interval = 1
     while (interval < get_child_count()):
+        
+        #Instance a new enemy ai
         var newEnemy = PreEnemy.instance()
         
-        
         newEnemy._ready()
+        #Add it as a child of spawnpoint so it may appear 
         get_child(interval).add_child(newEnemy)
-        newEnemy.grip.get_child(0).queue_free()
+        
+        #We need to be sure we have the proper weapon, default will not cut it.
+        #We need to grab the node, unparent it and remove it before we add any new child.
+        var ch = newEnemy.grip.get_child(0)
+        newEnemy.grip.remove_child(ch)
+        ch.queue_free()
+        
+        #Now we can add a weapon to the enemy's grip without worries that the slot is taken
         newEnemy.grip.add_child(BulletFactory.get_weapon(get_child(interval).spawn_weapon))
+        #This SHOULD be correct now due to the child removal from before.
+        newEnemy.ai.weapon = newEnemy.grip.get_child(0)
+        
+        #We apparently need to use global position because position will offset it by thousands. How? IDK?
         newEnemy.global_position = get_child(interval).global_position
+        
+        #IDK if this is redundant but apparently having this here let's them spawn so... whatever.
         newEnemy.ai.origin_location = newEnemy.global_position
-        newEnemy.speed = 500
+        
+        #Now please acquire the player.
+        newEnemy.ai.accqu_player(Global.get_player())
         newEnemy.show()
         
+        #We need to also assing a beacon to them, 
         var newBeacon = PreBeacon.instance()
+        
+        #The parameters need to be set, ids must be the same in order to count.
         newBeacon.objective_id = id
         newBeacon.beacon_type = 0
         newBeacon.beacon_priority = 2
-        newEnemy.add_child(newBeacon)
         
-        beacons.push_back(newBeacon.get_name())
+        #New enemy no longer is relevant, we just need to parent it with the enemy to make the beacon despawn on death.
+        get_child(interval).get_child(0).add_child(newBeacon)
+        
+        beacons.push_back(newBeacon.name)
         interval = interval + 1
         pass
         
@@ -72,5 +94,4 @@ func _OnRevealed():
                 node.get_parent().speed = 500
                 
     
-    gotPlayer = true
     pass
